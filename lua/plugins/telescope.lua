4970e8129903a0e781674b45fc1173d4854bb7ac
nitishxyz <nitishxyz@proton.me>
2024-09-06 00:36:26 +0530

    update: telescope config to use rg

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,29 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-     dependencies = { 'nvim-lua/plenary.nvim' },
-     config = function()
-	local telescope = require("telescope");
-
-	telescope.setup({
-		extensions = {
-			["ui-select"] = {
-				require("telescope.themes").get_dropdown({}),
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local telescope = require("telescope")
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = { "node_modules" },
 			},
-		},
-	})
-
-	local builtin = require("telescope.builtin")
-	vim.keymap.set("n", "<leader>p", builtin.find_files, {})
-	vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-	-- require("telescope").load_extension("ui-select")
-	-- require("telescope").load_extension("lazygit")
-     end
+			pickers = {
+				find_files = {
+					hidden = true,
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+			},
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({}),
+				},
+			},
+		})
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		-- require("telescope").load_extension("ui-select")
+		require("telescope").load_extension("lazygit")
+	end,
 }
