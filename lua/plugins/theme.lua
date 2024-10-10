c2e5f4d65d2d68ed1553e20a18410a20b6e2d5e1
nitishxyz <nitishxyz@proton.me>
2024-10-10 19:01:06 +0530

    conform copilot and mini stuff

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -14,7 +14,7 @@ return {
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			-- load the colorscheme here
+			require("rose-pine").setup({})
 			vim.cmd([[colorscheme rose-pine]])
 		end,
 	},
